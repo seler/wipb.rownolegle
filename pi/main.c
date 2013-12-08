809fef263a5e8cb42068723897484b7476c00b0d
Rafał Selewońko <rafal@selewonko.com>
2013-12-08 11:34:35 +0100

    pi template

---
+++ pi/main.c
@@ -0,0 +1,10 @@
+// Copyright (c) 2013 Rafał Selewońko <rafal@selewonko.com>.
+
+#define ILE 65536
+#define BUFFER_SIZE 1024
+#define WRITE_TO_FILE 1
+
+
+int main(int argc, char *argv[]){
+    return 0;
+}
