1a69dcac65b72fd24e3e5e851c3e1a917e366c0a
Rafał Selewońko <rafal@selewonko.com>
2013-12-08 15:14:24 +0100

    dziala rownolegle, ale chujowo

--- pi/main.c
+++ pi/main.c
@@ -4,23 +4,28 @@
 #include <omp.h>
 #include <time.h>
 
+double get_pi(int num_threads, int k_max);
+
 
 int main(int argc, char *argv[]) {
-	double k_max;
 	double pi;
-	double sum[2];
-	double timea, timeb;
+	pi = get_pi(atoi(argv[1]), atoi(argv[2]) * 1000000.);
+
+	//fprintf(stdout, "%s\n", "3.141592653589793238462643383279502884197169399375");
+	//fprintf(stdout, "%1.48f\n", pi);
+	return 0;
+}
+
+double get_pi(int num_threads, int k_max){
+	double pi;
+	double sum[num_threads];
 	int i;
-	int num_threads;
 	int nthreads;
 
 	pi = 0.;
-	k_max = (double)atoi(argv[2]) * 1000000.;
 
-	num_threads = atoi(argv[1]);
 	omp_set_num_threads(num_threads);
 
-	timea = clock();
 	#pragma omp parallel
 	{
 		double i;
@@ -29,19 +34,17 @@ int main(int argc, char *argv[]) {
 		if (id == 0) {
 			nthreads = nthrds;
 		}
+		sum[id] = 0.0;
 
 		for(i = id; i <= k_max; i+=nthrds)
 		{
 			sum[id] += 4 * (pow(-1, i)/((2 * i) + 1));
 		}
 	}
+
 	pi = 0.0;
 	for(i = 0; i < nthreads; i++){
-		pi += sum[i]
+		pi += sum[i];
 	}
-	timeb = clock();
-
-	//fprintf(stdout, "%s\n", "3.141592653589793238462643383279502884197169399375");
-	fprintf(stdout, "%f, %d, %1.48f\n", (double)(timeb - timea) / CLOCKS_PER_SEC, num_threads, pi);
-	return 0;
+	return pi;
 }
